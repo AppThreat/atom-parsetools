714b83b078c9d08c7714c0532328eb766f597cac
Prabhu Subramanian <prabhu@appthreat.com>
2026-02-02 12:15:05 +0100

    Include composer json and composer lock
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ if command -v php >/dev/null 2>&1; then
   php -r "unlink('composer-setup.php');"
   export COMPOSER_VENDOR_DIR=plugins
   php composer.phar require nikic/php-parser:5.7.0 --ignore-platform-reqs --optimize-autoloader
-  rm composer.phar composer.json composer.lock
+  rm composer.phar
 else
   echo "PHP plugins not built."
 fi
